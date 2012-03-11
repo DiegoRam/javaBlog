bdcb9fb2f595498700c953b264636107c26d9467
Diego Ramirez <diego@eva01.(none)>
2012-03-11 20:23:57 -0300

    add form comment function

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,11 +1,15 @@
 package controllers;
 
+import java.sql.Date;
 import java.util.List;
 
 import play.*;
+import play.data.validation.Required;
 import play.mvc.*;
 import play.mvc.results.NotFound;
+import models.Comment;
 import models.Post;
+import models.User;
 
 
 public class Application extends Controller {
@@ -32,5 +36,18 @@ public class Application extends Controller {
 			render(post);
 		}
 	}
+    
+    public static void saveComment( Long postId, @Required String firstname, @Required String texto){
+    	
+    	Post post = Post.findById(postId);
+    	
+    	Comment comment = new Comment(post, null,firstname, texto, new Date(System.currentTimeMillis()));
+    	if (validation.hasErrors()){
+    		render("Application/viewPost.html",post);
+    	}
+    	comment.save();
+    	flash.success("Gracia Amigo!");
+    	viewPost(postId);
+    }
 
 }
--- app/models/Comment.java
+++ app/models/Comment.java
@@ -6,17 +6,22 @@ import javax.persistence.Entity;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 
+import play.data.validation.Required;
 import play.db.jpa.Model;
 @Entity
 public class Comment extends Model {
 	
 	public Date postedat;
 	@Lob
+	@Required
 	public String text ;
 	@ManyToOne
 	public Post post;
 	@ManyToOne
 	public User author;
+	@Required
+	public String author_firstname;
+	
 	/**
 	 * 
 	 * @param post
@@ -24,16 +29,21 @@ public class Comment extends Model {
 	 * @param text
 	 * @param postedat
 	 */
-	public Comment(Post post, User author, String text, Date postedat) {
+	public Comment(Post post, User author, String author_name, String text, Date postedat) {
 		super();
 		this.post = post;
 		this.author = author;
 		this.text = text;
 		this.postedat = postedat;
+		this.author_firstname = author_name ;
 	}
 	@Override
 	public String toString(){
+		if (this.author != null){
 		return author.firstname + ": " + text ;
+		} else {
+			return this.author_firstname + ": " + text;
+		}
 	}
 	
 	
